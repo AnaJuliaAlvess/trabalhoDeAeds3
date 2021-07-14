9e5bfa4a75723797c1d9d2c96632ff415b4e1374
Aline Dias <martinsaline558@gmail.com>
2021-07-14 17:56:21 -0300

    letra b

--- bibliotecaGrafo.py
+++ bibliotecaGrafo.py
@@ -1,20 +1,57 @@
-#def lista_adjacencia(listaArquivo):
+def listaAdjacencia(arquivo,vertices,arestas):
+    listaAd = [[] for i in range(vertices)]  # cria lista
+    matrizAd = [[0 for i in range(vertices)] for i in range(vertices)]
+    v = [i for i in range(vertices)] #vetor vertices
+    a = [] #vetor com arestas
+    for i in range(arestas):
+        l = arquivo.readline()
+        lInt= list(map(int, (l.split(' '))))
+        origem= int(lInt[0])
+        destino= int(lInt[1])
+        peso=int(lInt[2])
+        listaAd[origem].append((destino, peso))
+        listaAd[destino].append((origem, peso))
+        a.append((origem, destino, peso))
+        matrizAd[origem][destino] = peso
+        matrizAd[destino][origem] = peso
 
+    for i in range(vertices):
+        for j in range(vertices):
+            print(matrizAd[i][j], end= " ")
+        print()
+    return listaAd
 
+def matriz_adjacencia(arquivo,vertices,arestas):
+    listaAd = [[] for i in range(vertices)]  # cria lista
+    matrizAd = [[0 for i in range(vertices)] for i in range(vertices)]
+    v = [i for i in range(vertices)]  # vetor vertices
+    a = []  # vetor com arestas
+    for i in range(arestas):
+        l = arquivo.readline()
+        lInt = list(map(int, (l.split(' '))))
+        origem = int(lInt[0])
+        destino = int(lInt[1])
+        peso = int(lInt[2])
+        listaAd[origem].append((destino, peso))
+        listaAd[destino].append((origem, peso))
+        a.append((origem, destino, peso))
+        matrizAd[origem][destino] = peso
+        matrizAd[destino][origem] = peso
 
+    for i in range(vertices):
+        for j in range(vertices):
+            print(matrizAd[i][j], end=" ")
+        print()
+    return matrizAd
 
 nome_arquivo = input("Digite o nome do arquivo com a sua extensão:")
-manipulador = open(nome_arquivo, "r")
-print(manipulador.read()) #imprime a lista só para  verificar se abriu
-manipulador.seek(0) #volta para o inicio do arquivo
-lista_arquivo= manipulador.readlines() #cria uma lista com os elementos do arquivo .txt
-lista_grafo= []
-for i in lista_arquivo:
-     for e in i:
-         if  e == '\n':
-             break;
-         else:
-             lista_grafo.index(e)
-
-print(lista_arquivo)
-print (lista_grafo)
+manipulador= open(nome_arquivo, "r") # colocar uma mensagem de erro se nao abrir o arquivo
+linha= manipulador.readline() #pega a linha de aresta e vertice
+linhaInt= list(map(int, (linha.split(' '))))
+vertice= int(linhaInt[0])
+aresta=int(linhaInt[1])
+op = int(input ("Digite 1 para representar o grafo como lista de adjacencia  ou 2  para representar como matriz de adjacência:"))
+if op == 1:
+    print(listaAdjacencia(manipulador,vertice,aresta))
+else:
+    matriz_adjacencia(manipulador, vertice,  aresta)
