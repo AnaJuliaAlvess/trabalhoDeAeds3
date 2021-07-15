802cd8e699c1f52c9c6d3c440c92c529796814ba
Aline Dias <martinsaline558@gmail.com>
2021-07-14 22:00:16 -0300

    atualização letra b

--- bibliotecaGrafo.py
+++ bibliotecaGrafo.py
@@ -1,29 +1,6 @@
-def listaAdjacencia(arquivo,vertices,arestas):
+def representacao(arquivo, vertices, arestas, opcao):
     listaAd = [[] for i in range(vertices)]  # cria lista
-    matrizAd = [[0 for i in range(vertices)] for i in range(vertices)]
-    v = [i for i in range(vertices)] #vetor vertices
-    a = [] #vetor com arestas
-    for i in range(arestas):
-        l = arquivo.readline()
-        lInt= list(map(int, (l.split(' '))))
-        origem= int(lInt[0])
-        destino= int(lInt[1])
-        peso=int(lInt[2])
-        listaAd[origem].append((destino, peso))
-        listaAd[destino].append((origem, peso))
-        a.append((origem, destino, peso))
-        matrizAd[origem][destino] = peso
-        matrizAd[destino][origem] = peso
-
-    for i in range(vertices):
-        for j in range(vertices):
-            print(matrizAd[i][j], end= " ")
-        print()
-    return listaAd
-
-def matriz_adjacencia(arquivo,vertices,arestas):
-    listaAd = [[] for i in range(vertices)]  # cria lista
-    matrizAd = [[0 for i in range(vertices)] for i in range(vertices)]
+    matrizAd = [[0 for i in range(vertices)] for i in range(vertices)]  # cria matriz
     v = [i for i in range(vertices)]  # vetor vertices
     a = []  # vetor com arestas
     for i in range(arestas):
@@ -37,12 +14,13 @@ def matriz_adjacencia(arquivo,vertices,arestas):
         a.append((origem, destino, peso))
         matrizAd[origem][destino] = peso
         matrizAd[destino][origem] = peso
+    if opcao== 1:
+        return listaAd
+    if opcao == 2:
+        return matrizAd
 
-    for i in range(vertices):
-        for j in range(vertices):
-            print(matrizAd[i][j], end=" ")
-        print()
-    return matrizAd
+    return -1
+
 
 nome_arquivo = input("Digite o nome do arquivo com a sua extensão:")
 manipulador= open(nome_arquivo, "r") # colocar uma mensagem de erro se nao abrir o arquivo
@@ -51,7 +29,5 @@ linhaInt= list(map(int, (linha.split(' '))))
 vertice= int(linhaInt[0])
 aresta=int(linhaInt[1])
 op = int(input ("Digite 1 para representar o grafo como lista de adjacencia  ou 2  para representar como matriz de adjacência:"))
-if op == 1:
-    print(listaAdjacencia(manipulador,vertice,aresta))
-else:
-    matriz_adjacencia(manipulador, vertice,  aresta)
+rep= representacao(manipulador,vertice,aresta,op)
+print(rep)
